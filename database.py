3a235c63883aad73c49fca8721d87f73fea56f92
VeerX-27G <gurveersinghx6@gmail.com>
2026-09-08 11:09:04 -0600

    updated database

--- database.py
+++ database.py
@@ -1,10 +1,21 @@
 from sqlalchemy import create_engine
-
+import os
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./items.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+if SQLALCHEMY_DATABASE_URL:
+    # Heroku outputs "postgres://", but SQLAlchemy requires "postgresql://"
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    # Fallback to local SQLite when running on your local machine
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
